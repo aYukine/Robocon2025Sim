1d1561d08a18c8a475daa9f5f84f358a54016ac2
aYukine <phayuk168@gmail.com>
2024-10-16 13:09:37 +0700

    rotate vector function

--- ultil.py
+++ ultil.py
@@ -1,5 +1,13 @@
 import math
+import numpy as np
 
 def find_rotation(x, y, r, degree):
   radian = (degree/180)*math.pi
   return x+ r*math.cos(radian), y + r*math.sin(radian)
+
+def find_vector_rotation(vector):
+  x, y = vector
+  angle_rad = np.arctan2(y, x)
+  angle_deg = np.degrees(angle_rad)
+  return angle_deg
+
